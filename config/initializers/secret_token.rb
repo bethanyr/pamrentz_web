e9136fc2038fb554ab1ab05b054eaf91a953a703
bethanyr <bethany.rentz@hotmail.com>
2014-08-09 08:16:17 -0700

    initial version of website

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PamrentzWeb::Application.config.secret_key_base = 'a0b9d6f180511f888d97343f244fb9435256d9da9025144e8b095ffe4cb725c8fe97585b76b8a1507842ff9b7f1fe0c57ad398bdd78461c899491e05df5ddd7a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PamrentzWeb::Application.config.session_store :cookie_store, key: '_pamrentz_web_session'
